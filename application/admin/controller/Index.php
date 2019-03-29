676554fac49d4d95e6ec9febb60896a9ae99b480
fengzi930610 <1057780049@qq.com>
2019-03-29 17:19:38 +0800

    后台首页修改

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,7 +4,18 @@ use app\admin\controller\Commond;
 	
 	class Index extends Commond{
 		public function index(){
-			$this->assign('title','后台首页');
+			// print_r($_SERVER);die;
+			if( (boolean) session('username')){
+				$where = array('username'=>session('username'));
+				$items = db("manager")->where($where)->find();
+				$items['last_time'] = date('Y-m-d  H:i:s', $items['last_time']);
+				// print_r($items);die;
+			}
+			// $items = db->
+			$_SERVER['REQUEST_TIME_FLOAT'] = date('Y-m-d  H:i:s', $_SERVER['REQUEST_TIME_FLOAT']);
+			$this->assign('title','赫曼商城后台首页');
+			$this->assign('items',$items);
+			$this->assign('server',$_SERVER);
 			return $this->fetch();
 		}
 	}
